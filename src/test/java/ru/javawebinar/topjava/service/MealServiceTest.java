61a7c1b8d27ffaa96095a4b7948c9d26fb9689b1
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-10-23 17:16:33 +0300

    HW3_4

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,46 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MealServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getBetweenDates() {
+    }
+
+    @Test
+    public void getBetweenDateTimes() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void create() {
+    }
+}
