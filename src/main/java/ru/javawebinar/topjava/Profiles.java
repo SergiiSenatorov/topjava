0d71ac4f24966d492a155fca32e496e9e31527d2
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-11-06 11:01:58 +0200

    HW5_Start

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -8,6 +8,7 @@ public class Profiles {
 
     public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
 
+
     public static final String
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
