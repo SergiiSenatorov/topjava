d3a612cdb1b796a010a3fbf130edc2dc0d61026b
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-11-06 14:08:36 +0200

    HW5_1

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -2,8 +2,10 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,30 +14,39 @@ import java.util.List;
 public class DataJpaMealRepositoryImpl implements MealRepository {
 
     @Autowired
-    private CrudMealRepository crudRepository;
+    private CrudMealRepository mealRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
-    public Meal save(Meal Meal, int userId) {
-        return null;
+    @Transactional
+    public Meal save(Meal meal, int userId) {
+        if(!meal.isNew() && get(meal.getId(),userId) == null)
+            return null;
+        meal.setUser(userRepository.get(userId));
+        return mealRepository.save(meal);
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        //return mealRepository.delete(id, userId);
+        return (mealRepository.deleteByIdAndUser(id, userRepository.get(userId)) != null);
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return mealRepository.get(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return mealRepository.findAllByUserOrderByDateTimeDesc(userRepository.get(userId));
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return mealRepository.findAllByUserAndDateTimeBetweenOrderByDateTimeDesc(userRepository.get(userId), startDate, endDate);
     }
 }
